7f03ae3693ef4a145c658b8c566332c0c1689346
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-12-04 13:19:53 +0100

    fixed drivers being stupid

--- SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
+++ SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
@@ -1,19 +1,34 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices.ComTypes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
 
 namespace SeleniumTestRNGolf
 {
     [TestClass]
     public class SeleniumTest
     {
-        IWebDriver driver = new ChromeDriver("http://localhost:52549/api/swingdata");
-
+        
+        //disse drivers finder henholdsvis geckodriver og chromedriver,
+        //der er lagt sammen med projektets .cs og .csproj filer,
+        //ved at lede i appens base directory
+        IWebDriver firefoxDriver = new FirefoxDriver(AppContext.BaseDirectory);
+        IWebDriver chromeDriver = new ChromeDriver(AppContext.BaseDirectory);
 
         [TestMethod]
         public void TestMethod1()
         {
+            
 
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            firefoxDriver.Navigate().GoToUrl("http://localhost:3000/testpage.html");
 
         }
     }
