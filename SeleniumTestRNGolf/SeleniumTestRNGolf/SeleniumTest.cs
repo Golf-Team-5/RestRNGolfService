c27e0793fe5380fec268e45d929dad83c96cc874
Løven <43264940+mark-knudsen@users.noreply.github.com>
2019-12-04 11:38:32 +0100

    Tilføjet SeleniumTest

---
+++ SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumTestRNGolf
+{
+    [TestClass]
+    public class SeleniumTest
+    {
+        IWebDriver driver = new ChromeDriver("http://localhost:52549/api/swingdata");
+
+        [TestInitialize]
+        public void TestSetUp()
+        {
+            driver.Navigate().GoToUrl("https://");
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
