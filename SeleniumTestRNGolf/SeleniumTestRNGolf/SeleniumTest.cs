1dfa72bf4622989ee7069cd6d15e0d807c8cfca1
Rasmus Drotved Laursen <rasm781y@edu.easj.dk>
2019-12-05 10:24:05 +0100

    made testPageUrl

--- SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
+++ SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
@@ -11,23 +11,26 @@ namespace SeleniumTestRNGolf
     {
 
         //disse drivers finder henholdsvis geckodriver og chromedriver,
-        //der er lagt sammen med projektets .cs og .csproj filer,
+        //der er lagt i bin\debug\netcoreapp2.1,
         //ved at lede i appens base directory
+
         IWebDriver firefoxDriver = new FirefoxDriver(AppContext.BaseDirectory);
-        IWebDriver chromeDriver = new ChromeDriver(AppContext.BaseDirectory);
+        //IWebDriver chromeDriver = new ChromeDriver(AppContext.BaseDirectory);
+
+        string testPageUrl =  "http://localhost:3000/testpage.html";
 
         [TestMethod]
         public void TestMethod1()
         {
-            chromeDriver.Navigate().GoToUrl("https://www.google.com/");
-            chromeDriver.Quit();
+            firefoxDriver.Navigate().GoToUrl(testPageUrl);
+
         }
 
 
         [TestMethod]
         public void TestMethod2()
         {
-            firefoxDriver.Navigate().GoToUrl("https://www.google.com/");
+            firefoxDriver.Navigate().GoToUrl(testPageUrl);
             firefoxDriver.Quit();
         }
     }
