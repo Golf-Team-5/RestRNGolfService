c748df9058bc700ce9506205acaf3a0ab5de8210
Mark Svendstrup Knudsen <mark4610@edu.easj.dk>
2019-12-05 09:23:54 +0100

    Drivers works : )

--- SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
+++ SeleniumTestRNGolf/SeleniumTestRNGolf/SeleniumTest.cs
@@ -1,17 +1,15 @@
-﻿using System;
-using System.IO;
-using System.Runtime.InteropServices.ComTypes;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Firefox;
+using System;
 
 namespace SeleniumTestRNGolf
 {
     [TestClass]
     public class SeleniumTest
     {
-        
+
         //disse drivers finder henholdsvis geckodriver og chromedriver,
         //der er lagt sammen med projektets .cs og .csproj filer,
         //ved at lede i appens base directory
@@ -21,15 +19,15 @@ namespace SeleniumTestRNGolf
         [TestMethod]
         public void TestMethod1()
         {
-            
-
+            chromeDriver.Navigate().GoToUrl("https://www.google.com/");
+            chromeDriver.Quit();
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            firefoxDriver.Navigate().GoToUrl("http://localhost:3000/testpage.html");
-
+            firefoxDriver.Navigate().GoToUrl("https://www.google.com/");
+            firefoxDriver.Quit();
         }
     }
 }
